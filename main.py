2bd97cdfa3c1ecf13d9be92367c568f5cb014374
Alfreeeed <luis.barradas.513@gmail.com>
2026-05-23 23:01:52 -0700

    feat(core): crear archivo main base e implementar loop principal del juego

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hola Mundo!")
+print("base code")
+
+def main_loop():
+    print("main code")
+
+    def entity(name, life) -> dict:
+        return {
+            "name": name, 
+            "life": life
+        }
+    
+    player = entity("player", 100)
+
+    while player["life"] > 0:
+        print(f"{player['name']} has {player['life']} life left.")
+        damage = int(input(f"Enter damage taken: >\n"))
+        player["life"] -= damage
+
+        if  player["life"] <= 0:
+            print(f"{player['name']} has died.")
+            break
+
+    print("Game Over")
+    restart = input("Do you want to restart? (y/n) >\n")
+    if restart.lower() == "y":
+        main_loop()
+    else:
+        print("Thanks for playing!")
+if __name__ == "__main__":
+    main_loop()
